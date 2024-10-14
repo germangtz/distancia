ea4cd9bb68b1f7dbe065a17b1e407185bab4f8a2
germangtz <licgerman@gmail.com>
2024-10-14 08:03:00 -0600

    Se agregó el autor

--- distancia.cpp
+++ distancia.cpp
@@ -1,24 +1,18 @@
-// Source: https://usaco.guide/general/io
-
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
 	double x1, y1, x2, y2, d;
 	cout << "Programa para calcular la distancia entre dos puntos." << endl;
+	cout << "Autor: Juan Perez" << endl;
 	cout << "x1:";
 	cin >> x1;
 	cout << "y1:";
 	cin >> y1;
-
-
 	cout << "x2:";
 	cin >> x2;
 	cout << "y2:";
 	cin >> y2;
-
 	d=sqrt( (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
 	cout << "La distancia entre los dos puntos es: ";
 	cout << d;
 	cout << endl;
